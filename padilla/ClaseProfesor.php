d4b371362b31c53bc20df7787302413f9f11c863
Lele-Poncho <112735104+Lele-Poncho@users.noreply.github.com>
2022-10-30 23:22:40 -0300

    Add files via upload

---
+++ padilla/ClaseProfesor.php
@@ -0,0 +1,45 @@
+<?php
+	$con = new mysqli("localhost","root","","b_datos");
+	
+	class Profesor
+	{
+		private $conexionBD; //Se guarda la conexión a la base de datos
+
+		//Método para añadir un profesor
+		private function setProfe(){
+			
+		}
+		//Método para añadir la descripción del profe
+		private function setDescripcionProfe(){
+
+		}
+		//Método para añadir la descripción del auto
+		private function setDescripcionAuto(){
+
+		}
+		//Método para añadir la descripción del lugar
+		private function setDescripcionLugar(){
+
+		}
+		//Método para obtener un profesor
+		private function getProfesores(){
+
+		}
+		//Método para obtener la descripción de un profesor
+		private function getDescripcionProfe(){
+
+		}
+		//Método para obtener la descripción de un auto
+		private function getDescripcionAuto(){
+
+		}
+		//Método para obtener la descripción del lugar
+		private function getDescripcionLugar(){
+
+		}
+		//Método para eliminar el profesor
+		private function eliminarProfe(){
+			
+		}
+	}
+?>
